08891c462f40e62206252eac7ea370069c82ba2f
myui <yuin405@gmail.com>
2016-06-20 20:13:43 +0900

    Fixed to cd to WORKING_DIR

--- bin/build_xgboost.sh
+++ bin/build_xgboost.sh
@@ -3,7 +3,6 @@
 # Hivemall: Hive scalable Machine Learning Library
 #
 # Copyright (C) 2015 Makoto YUI
-# Copyright (C) 2013-2015 National Institute of Advanced Industrial Science and Technology (AIST)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,11 +24,12 @@ XGBOOST_HASHVAL='209f08176d677c6eed3343489858a7578472656d'
 
 # Move to a working directory
 WORKING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $WORKING_DIR
 
 # Final output dir for a custom-compiled xgboost binary
-HIVEMALL_LIB_DIR="$WORKING_DIR/../core/src/main/resources/lib/"
-rm -rf $HIVEMALL_LIB_DIR >> /dev/null
+HIVEMALL_LIB_DIR="$WORKING_DIR/../core/src/main/resources/lib"
 mkdir -p $HIVEMALL_LIB_DIR
+rm -f $HIVEMALL_LIB_DIR/* >> /dev/null
 
 # Move to an output directory
 XGBOOST_OUT="../target/xgboost-$XGBOOST_HASHVAL"
@@ -38,17 +38,10 @@ mkdir -p $XGBOOST_OUT
 cd $XGBOOST_OUT
 
 # Fetch xgboost sources
-git clone --progress https://github.com/maropu/xgboost.git
+git clone --recursive --progress https://github.com/maropu/xgboost.git
 cd xgboost
-
-# Resolve dependent sources
-git submodule init dmlc-core
-git submodule update dmlc-core
-git submodule init rabit
-git submodule update rabit
 
 # Copy a built binary to the output
 cd jvm-packages
 ./create_jni.sh
 cp ./lib/libxgboost4j.* "$HIVEMALL_LIB_DIR"
-
